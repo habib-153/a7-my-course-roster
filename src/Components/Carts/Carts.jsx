34fdca2fc544f9291fe21729df6cb0eee8eb621f
habib-153 <h.r.sihab155@gmail.com>
2023-09-15 03:42:19 +0600

    list style added

--- src/Components/Carts/Carts.jsx
+++ src/Components/Carts/Carts.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
 
-import Cart from "../Cart/Cart";
-
 
 const Carts = ({courses, addCredit, remainingCredit, totalPrice}) => {
     return (
@@ -10,13 +8,13 @@ const Carts = ({courses, addCredit, remainingCredit, totalPrice}) => {
                 <h3 className="text-[#2F80ED] pb-2 border-b-2 font-bold text-lg">Course Hour Remaining {remainingCredit}hr</h3>
                 <div>
                     <h3 className="font-bold text-lg my-2">Course Name</h3>
-                    <div className="pb-2 border-b-2 text-[rgba(28,27,27,0.6)]">
-                        <ul>
+                    <ul className="pb-2 ml-5 list-decimal border-b-2 text-[rgba(28,27,27,0.6)]">
+                        
                         {
-                            courses.map((course, idx) =><li key={idx}><Cart course={course}></Cart></li>)
+                            courses.map(course=><li key={course.id}>{course.title}</li>)
                         }
-                        </ul>
-                    </div>
+                    
+                    </ul>
                     <p className="py-2 text-[rgba(28,27,27,0.6)] font-semibold border-b-2">Total Credit Hour: {addCredit}</p>
                     <p className="py-2 text-[rgba(28,27,27,0.6)] font-semibold">Total Price: {totalPrice}</p>
                 </div>
--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import {FaBookOpen, FaDollarSign } from 'react-icons/fa';
 
